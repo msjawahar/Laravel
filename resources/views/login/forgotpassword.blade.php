cfb3dadcb7d048ba3d0b12c25d118a8b31438b59
msjawahar <msjawahar@gmail.com>
2015-12-09 11:00:11 +0530

    change

--- resources/views/login/forgotpassword.blade.php
+++ resources/views/login/forgotpassword.blade.php
@@ -20,7 +20,7 @@
 <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Register 1 2 3 4 5</h1>
+                    <h1 class="page-header">Register</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
